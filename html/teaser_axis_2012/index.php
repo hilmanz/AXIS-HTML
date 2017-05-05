c2d43e52919e7d9d689cfa1e859356990376527d
Rangga Herly Hilman Herawan <rangga.herly@kana.co.id>
2017-05-05 12:09:25 +0700

    KCM backup Sites
    Backup Initiation

---
+++ html/teaser_axis_2012/index.php
@@ -0,0 +1,85 @@
+<?php
+
+	class counter 
+	
+		{
+		
+				function __construct(){
+				
+					include "db.php";
+					$this->db = new db();
+					
+					
+				}				
+				
+				function hitcounter(){
+				
+					/*
+					$data = $this->checkmachinecounter();
+					if($data->total > 0) 
+					{
+						
+						print json_encode(false);
+						exit;
+					}
+					*/
+					
+					$type = intval($_POST['type']);
+					$sql ="
+						INSERT INTO 
+						axis_teaser_2012.teaser_data	(ipaddress,type,date,date_ts) 
+						VALUES ('{$_SERVER['REMOTE_ADDR']}',{$type},NOW(),".time().") ";
+						$this->db->query($sql);
+						print json_encode(true);
+					exit;
+				}
+				
+				function getcounter(){
+
+					$sql ="SELECT count(*)total,type FROM axis_teaser_2012.teaser_data GROUP BY type; ";
+					$data = $this->db->fetch($sql,true);
+					return $data;
+				}
+				
+				function checkmachinecounter(){
+				
+					$sql ="SELECT count(*) total FROM axis_teaser_2012.teaser_data WHERE ipaddress = '{$_SERVER['REMOTE_ADDR']}'  ";
+					$data = $this->db->fetch($sql);
+					return $data;
+				}
+				
+				function getTime(){
+					$LAUNCHING_DATE = strtotime('2012-12-12 12:12:00');
+					$launchdate = array('day'=>intval(date("d",$LAUNCHING_DATE)),
+						'month'=>intval(date("m",$LAUNCHING_DATE)),
+						'year'=>intval(date("Y",$LAUNCHING_DATE)),
+						'hour'=>intval(date("H",$LAUNCHING_DATE)),
+						'min'=>intval(date("i",$LAUNCHING_DATE)),
+						'sec'=>intval(date("s",$LAUNCHING_DATE))
+						);
+					return json_encode($launchdate);
+					
+				}
+		}
+		
+	$arr['count0'] = 0;
+	$arr['count1'] = 0;
+	$teaser = new counter();
+	if(array_key_exists('act',$_POST)){
+		if($_POST['act']=='hitcounter'){
+			$teaser->hitcounter();
+			
+		}
+		
+	}
+	$launchts = $teaser->getTime();		
+	$dteaser = $teaser->getcounter();	
+	if($dteaser){
+		foreach($dteaser as $val){
+			$arr["count".$val->type]=$val->total;
+		}
+	}
+
+	include "teaser.php";
+	
+?>
---
+++ html/thewinner.php
@@ -0,0 +1,152 @@
+<div id="breadcumb" class="wrapper">
+	<h1><a href="index.php">Home</a> &raquo; <a>100 Uploader PERTAMA</a></h1>
+</div><!-- end #breadcumb -->
+<div id="container">
+    <div class="wrapper" id="axisLife">
+    	<div id="bgSplash">
+            <div id="theWinner">
+            	<div class="theWinner">
+           	 		<div class="theWinnerBottom">
+                        <div class="winnLogo">
+                            <img src="images/win_logo_id.png" />
+                        </div>
+                        <script>
+							jQuery(document).ready(function($) {
+								$('#selectDates').change(function(){
+									var selected = $(this).find(':selected');
+									$('.winnerList').fadeOut();
+									$('.winnerList').removeClass("tabActive");
+								    $('.'+selected.val()).fadeIn();
+								    $('.'+selected.val()).addClass("tabActive");
+								});
+							});
+						</script>
+                        <h1 class="winTitle">100 Uploader PERTAMA</h1>
+                        <h3 class="selectPeriod">Periode Pemenang :</h3>
+                        <select id="selectDates" class="styled">
+                                <option value="date-17112012">17 November 2012</option>
+                                <option value="date-18112012">18 November 2012</option>
+                                <option value="date-19112012">19 November 2012</option>
+                        </select>
+                        <div id="tabwinnner">
+                            <div class="winContent winnerList date-17112012 tabActive">
+                                <ol class="colLeft">
+                                  <li><a href="https://plus.google.com/105121862834824388606">Joeza Eza</a></li>
+                                  <li><a href="https://plus.google.com/118141497364653909454">Monang Sianipar</a></li>
+                                  <li><a href="https://plus.google.com/101661984126121001746">Windy Andrianiaisah</a></li>
+                                </ol>
+                            </div><!-- end .winContent -->
+                            <div class="winContent winnerList date-18112012">
+                                <ol class="colLeft">
+                                  <li><a href="https://plus.google.com/100758288443705667556">Heri Setiawan</a></li>
+                                  <li><a href="https://plus.google.com/103254309802604883074">Bhonsay Taimiong</a></li>
+                                  <li><a href="https://plus.google.com/108885210760040184111">Ecy Cecem</a></li>
+                                  <li><a href="https://plus.google.com/114292063823415717543">Suci Chixmal</a></li>
+                                  <li><a href="https://plus.google.com/106014614338844077046">Mahendra Putra</a></li>
+                                  <li><a href="https://plus.google.com/107344843289240146157">Faisal Acill</a></li>
+                                  <li><a href="https://plus.google.com/114109534116079460594">Mizz Sunshine</a></li>
+                                  <li><a href="https://plus.google.com/112575158348412858712">Rudi Hariono</a></li>
+                                  <li><a href="https://plus.google.com/109381150841070920536">Rama Makie</a></li>
+                                  <li><a href="https://plus.google.com/117609155394042189871">Arifah Abriana Nasution</a></li>
+                                </ol>
+                                <ol class="colLeft" start="11">
+                                  <li><a href="https://plus.google.com/113553372284558990453">Tomi Yuli Setiawan</a></li>
+                                  <li><a href="https://plus.google.com/101875616297883539405">Ircham Gallan</a></li>
+                                  <li><a href="https://plus.google.com/102925098593308881898">Chafidin Chaf</a></li>
+                                  <li><a href="https://plus.google.com/100883577737414368936">Miftahul Jannah</a></li>
+                                  <li><a href="https://plus.google.com/107087527763128805415">Tonugamu </a>S </li>
+                                  <li><a href="https://plus.google.com/101658212288378623546">Yosef Wahyudhi</a></li>
+                                  <li><a href="https://plus.google.com/114237780711052681568">Rima Pramanik</a></li>
+                                  <li><a href="https://plus.google.com/113099848198731058448">Ali Ridho</a></li>
+                                </ol>
+                            </div><!-- end .winContent -->
+                            <div class="winContent winnerList date-19112012">
+                                <ol class="colLeft">
+                                  <li><a href="https://plus.google.com/114109534116079460594">Mizz Sunshine</a></li>
+                                  <li><a href="https://plus.google.com/112575158348412858712">Rudi Hariono</a></li>
+                                  <li><a href="https://plus.google.com/109381150841070920536">Rama Makie</a></li>
+                                  <li><a href="https://plus.google.com/117609155394042189871">Arifah Abriana Nasution</a></li>
+                                  <li><a href="https://plus.google.com/113553372284558990453">Tomi Yuli Setiawan</a></li>
+                                  <li><a href="https://plus.google.com/101875616297883539405">Ircham Gallan</a></li>
+                                  <li><a href="https://plus.google.com/102925098593308881898">Chafidin Chaf</a></li>
+                                  <li><a href="https://plus.google.com/100883577737414368936">Miftahul Jannah</a></li>
+                                  <li><a href="https://plus.google.com/107087527763128805415">Tonugamu S</a></li>
+                                  <li><a href="https://plus.google.com/101658212288378623546">Yosef Wahyudhi</a></li>
+                                </ol>
+                            </div><!-- end .winContent -->
+                        </div><!-- end #tabsingle -->
+                        <div class="winContent" id="footerWinner">
+                        <p>Jika namamu tertera pada daftar di atas, mohon segera kirimkan data diri ke <a href="mailto:digital@axisworld.co.id">digital@axisworld.co.id</a> 
+                            dengan alamat email yang kamu gunakan untuk akun G+ kamu.
+                            Cukup cantumkan nama, nomor telepon dan nama akun G+ kamu.</p>
+                            <p>Lihat di sini untuk <a href="#TncPhotoUpload" class="showPopup">Syarat dan Ketentuan.</a></p>
+                        </div><!-- end .winContent -->
+                        <div class="socialLike">
+                            <a class="favorite">&nbsp;<span class="fav-count">3</span></a>
+                            <div class="facebookLike">
+                                <div id="fb-root"></div>
+                                <script>(function(d, s, id) {
+                                  var js, fjs = d.getElementsByTagName(s)[0];
+                                  if (d.getElementById(id)) return;
+                                  js = d.createElement(s); js.id = id;
+                                  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=267918769990528";
+                                  fjs.parentNode.insertBefore(js, fjs);
+                                }(document, 'script', 'facebook-jssdk'));</script>
+                                <div class="fb-like" data-href="http://axisworld.co.id/" data-send="false" data-layout="button_count" data-width="120" data-show-faces="false"></div>
+                            </div><!-- end .facebookLike -->
+                            <div class="shareTwit">
+                                <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://axisworld.co.id/" data-via="axisgsm" data-related="axisgsm" data-hashtags="AXISGSM">Tweet</a>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+                            </div><!-- end .shareTwit -->
+                            <div class="googlePlus">
+                                <script type="text/javascript" src="https://apis.google.com/js/plusone.js">
+                                  {parsetags: 'explicit'}
+                                </script>
+                                <div class="g-plusone" data-size="medium" data-annotation="inline" data-width="120" data-href="http://axisworld.co.id/"></div>
+                                <script type="text/javascript">gapi.plusone.go();</script>
+                            </div><!-- end .googlePlus -->
+                        </div><!-- end .socialLike -->
+                	</div><!-- end .theWinnerBottom -->
+                </div><!-- end .theWinner -->
+            </div><!-- end #theWinner -->
+         </div>
+    </div><!-- end #axisLife -->
+</div><!-- end #container -->
+
+<div class="popup" id="TncPhotoUpload">
+    <div class="ui-overlay">
+        <div class="ui-widget-overlay"></div>
+    </div><!-- end .ui-overlay -->
+    <div class="popupContent" style="width:800px; margin:0 0 0 -400px; position:absolute;">
+    	<div class="headPopup">
+           	<h3>Syarat Dan Ketentuan</h3>
+        </div>
+        <div class="entry">
+			<ol>
+              <li>Dengan mengikuti kegiatan ini , maka  anda setuju dan menyatakan menerima syarat dan ketentuan yang berlaku. </li>
+              <li>Karyawan dan perwakilan dari PT AXIS Telekom Indonesia dan Google, afiliasinya, distributor, agen kreatif dan entitas lain yang bertindak atas nama AXIS dalam mengorganisir "AXIS G+ Photo Upload", serta kerabat dekat peserta, tidak memenuhi syarat dan atau tidak diperbolehkan untuk berpartisipasi dalam "AXIS G+ Photo Upload". </li>
+              <li>Masa pendaftaran AXIS G+ Photo Upload berlaku mulai tanggal 17 November 2012 sampai dengan tanggal 16 December 2012.</li>
+              <li>Photo yang diupload merupakan hak milik pribadi peserta  dan  AXIS maupun Google tidak bertanggung jawab apabila dikemudian hari terjadi penggugatan atau urusan hukum lain yang berhubungan dengan Photo maupun caption yang diupload atau diikutsertakan dalam pogram ini. </li>
+              <li>Peserta boleh mengirimkan sebanyak-banyaknya photo dengan caption dengan tema "Hangout Seru Bareng teman" yang unik sesuai ketentuan, namun setiap peserta hanya berhak 1 kali apresiasi selama periode program berlangsung.</li>
+              <li>Photo yang diupload tidak boleh mengandung unsur SARA, Pornografi, serta lolos proses Compliance dari AXIS dan Google. </li>
+              <li>100 orang pertama dengan foto yang lolos compliance berhak mendapatkan voucher pulsa isi ulang AXIS sebesar Rp.50.000,- dari AXIS dan Google.</li>
+              <li>Pengumuman 100 peserta pertama yang meng-up load photo sesuai dengan ketentuan akan di umumkan di  <a href="http://www.axisworld.co.id/GooglePlus" target="_blank">www.axisworld.co.id/GooglePlus</a> serta streamline akun Google+ "AXISGSM Yang Baik" sehari setelah proses penyaringan.</li>
+              <li>Setiap partisipan, diharapkan agar men-cek website <a href="http://www.axisworld.co.id/GooglePlus" target="_blank">www.axisworld.co.id/GooglePlus</a> atau streamline akun Google+ "AXISGSM Yang Baik". Bila nama anda tercantum, maka partisipan diharapkan mengirimkan kembali email (menggunakan email yang sama dengan yang digunakan pada akun google+) ke alamat <a href="mailto:digital@axisworld.co.id">digital@axisworld.co.id</a> berisikan bukti foto, nama (sesuai KTP), dan nomor handphone yang dapat dihubungi untuk proses verifikasi.</li>
+              <li>Setelah proses verifikasi, AXIS dan Google akan mengirimkan apresiasi kepada partisipan melalu E-mail.</li>
+              <li>Semua Photo yang terpilih akan menjadi hak milik tunggal AXIS dan Google, dengan hak kepemilikan penuh dan tak terkekang, termasuk namun tidak terbatas pada hak untuk membuat, mengedit, menerbitkan dan menggunakan bahan-bahan dan informasi di dalamnya tanpa pertimbangan lebih lanjut atau pembayaran tambahan kepada peserta. </li>
+              <li> Semua keputusan oleh atau atas nama AXIS dan Google dalam hal yang berkaitan dengan pemilihan photo adalah final dan tidak dapat diganggu gugat.</li>
+              <li>Segala bentuk apresiasi tidak dapat ditukar dengan apapun atau diuangkan.</li>
+              <li>Pengiriman tanda terimakasih (tanda apresiasi) akan dilakukan oleh pihak AXIS selambat-lambatnya dalam kurun waktu 30 hari setelah peserta terkait melengkapi ketentuan data-data yang diperlukan.</li>
+              <li> Semua data harus dikirimkan oleh peserta kepada AXIS paling lambat seminggu setelah pengumuman di website. AXIS dan Google berhak untuk menganulir segala bentuk apresiasi  jika peserta tidak mengirimkan data yang diminta dari jangka waktu yang ditentukan.</li>
+              <li>Peserta dalam AXIS G+ Photo akan memberikan persetujuan kepada AXIS dan Google untuk: 
+                <ol>
+                  <li>Menyimpan dan menggunakan informasi pribadi peserta dan mematuhi kebijakan internal AXIS dan Google. </li>
+                  <li> Menghubungi peserta dengan menggunakan data tersebut bila sewaktu-waktu diperlukan.</li>
+                  <li>AXIS dan Google tidak diperbolehkan untuk mengungkapkan informasi pribadi yang diterima dari peserta kepada pihak ketiga untuk tujuan lain. </li>
+                </ol>
+              </li>
+              <li>Peserta dapat membatalkan persetujuan untuk semua ini, atau  untuk tidak dihubungi setiap saat dengan mengirimkan email ke <a href="mailto:digital@axisworld.co.id">digital@axisworld.co.id</a> dengan subject pembatalan partisipasi dan detail  kontak.</li>
+            </ol>
+        </div><!-- end .entry -->
+    </div><!-- end .popupContent -->
+</div><!-- end .popup -->
